7487b10f20d84dcf19e9b45f37f64958cd5c4e36
kimminjae <khwhj3577@gmail.com>
2025-08-19 18:20:42 +0900

    day04_spring

---
+++ spring_5webmvc/src/main/java/org/iclass/spring_3mybatis/mapper/BuyMapper.java
@@ -0,0 +1,29 @@
+package org.iclass.spring_3mybatis.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.iclass.spring_3mybatis.dto.BuyDto;
+import org.iclass.spring_3mybatis.dto.CustomerBuyDto;
+import org.springframework.stereotype.Component;
+
+@Mapper
+public interface BuyMapper {
+    List<BuyDto> selectByCustomer(String customer_id);
+
+    List<BuyDto> selectByPcode(String pcode);
+
+    List<BuyDto> selectByYear(String year); // 내 방식은 int year
+
+    int selectSumByPcode(String pcode);
+
+    // join
+    List<CustomerBuyDto> selectSaleByCustomer(String customer_idString);
+
+    // count 집계함수
+    Map<String, Integer> selectCountByYear(String year);
+
+    List<Map<String, Object>> selectAllCountByYear();
+
+}
