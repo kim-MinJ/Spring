64749b198adc613819a6dd360d187e802470774d
kimminjae <khwhj3577@gmail.com>
2025-08-21 18:32:51 +0900

    day06_spring

---
+++ spring_6board/src/main/java/org/iclass/board/controller/APICommentsController.java
@@ -0,0 +1,17 @@
+package org.iclass.board.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+public class APICommentsController {
+    /*
+     * URL
+     * /api/comments -> 댓글 추가
+     * /api/comments/{idx} -> 댓글 삭제
+     * /api/comments/{mref} -> 메인글 댓글 목록
+     */
+
+}
---
+++ spring_6board/src/main/java/org/iclass/board/dto/PageResponseDTO.java
@@ -0,0 +1,28 @@
+package org.iclass.board.dto;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Builder
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class PageResponseDTO {
+	private int totalCount;		//전체 글 개수
+	private int totalPages;
+	
+	// 요청 페이지 기준으로 페이지 목록의 시작,끝 번호
+	private int startPage;
+	private int endPage;
+	
+	private List<CommunityDTO> list;    // 요청 페이지의 글목록
+
+}
